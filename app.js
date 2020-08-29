9b04029daba5b5309c7247c2ff1cde14a3dc1308
KatsuyaAkasaka <akasakatora1208@gmail.com>
2020-08-29 18:06:07 +0900

    add join and broadcast

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 7000;
+
+app.get('/' , (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.sockets.on('connection', (socket) => {
+  socket.on('join', (msg) => {
+    socket.join(msg);
+  });
+  socket.on('shake', (msg) => {
+    socket.broadcast.to(msg.room_id).emit(msg.time);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log('server listening. Port:' + PORT);
+});
